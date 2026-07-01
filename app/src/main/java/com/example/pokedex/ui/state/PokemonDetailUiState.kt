91b101412d65572da2275e47613a10fa58376689
tranthienlong675 <tranthienlong675@gmail.com>
2026-07-01 11:14:20 +0700

    Remove unused property: backgroundColor

--- app/src/main/java/com/example/pokedex/ui/state/PokemonDetailUiState.kt
+++ app/src/main/java/com/example/pokedex/ui/state/PokemonDetailUiState.kt
@@ -1,12 +1,10 @@
 package com.example.pokedex.ui.state
 
-import androidx.compose.ui.graphics.Color
 import com.example.pokedex.model.Pokemon
 
 data class PokemonDetailUiState(
     val pokemon: Pokemon? = null,
     val pokemonForms: List<Pokemon> = emptyList(),
-    val backgroundColor: Color = Color.Gray,
     val isLoading: Boolean = false,
     val error: String? = null
 )
